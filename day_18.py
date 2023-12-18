c94dfcc4bdf9d80c2fd8398734c910cf0688d3d3
Samuel Huwiler <samuel.huwiler@gmx.ch>
2023-12-18 10:27:07 +0100

    Part Two needs another solution, z.B. von aussen nicht von innen

--- day_18.py
+++ day_18.py
@@ -2,15 +2,26 @@ from utils.all import *
 
 
 D: dict = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
+I2D: dict = {"0": "R", "1": "D", "2": "L", "3": "R"}
 
 
 def list2grid(l: list) -> dict:
     return {(x, y): l[x][y] for x in range(len(l)) for y in range(len(l[0]))}
 
 
-input_18 = read_input_line(18, sep="\n")
-#  input_18 = read_input_line("test_18", sep="\n")
+def extract_from_hex(s: str) -> tuple:
+    inp = s.split(" ")[2].strip("()#")
+    d = I2D[inp[-1]]
+    n = int(inp[:-1], 16)
+    return (d, n)
 
+
+input_18 = read_input_line(18, sep="\n")
+input_18 = read_input_line("test_18", sep="\n")
+
+# Part Two
+#  input_18 = mapl(extract_from_hex, input_18)
+# Part One
 input_18 = mapl(lambda x: (x.split(" ")[0], int(x.split(" ")[1])), input_18)
 
 tot = 0
@@ -53,7 +64,7 @@ for d, n in input_18:
         y += dy
 
 
-visit = deque([(1, 1)])
+visit = deque([(height // 2, lenght // 2)])
 seen: set = set()
 G = matrix
 
@@ -67,4 +78,4 @@ while visit:
     visit.extend(neighbors8_coords(x, y))
 
 
-print("Part One:", tot)
+print("Solution:", tot)
